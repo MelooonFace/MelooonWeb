d8bddcd8e94750c859c4ad7bc1fd45859440b73c
Jacob Marshall <jacob.christian.marshall@gmail.com>
2012-07-30 20:44:31 +1200

    Started to (briefly) document the Output class

--- Melooon/core/classes/Output.php
+++ Melooon/core/classes/Output.php
@@ -5,16 +5,25 @@
 		
 		private $output;
 		
+		/**
+		 * Doesnt do anything yet
+		 */
 		function __construct()
 		{
 			// Nothing yet :)
 		}
 		
+		/**
+		 * Gets the output at its current state
+		 */
 		public function get_output()
 		{
 			return $this->output;
 		}
 		
+		/**
+		 * Sets the entire output
+		 */
 		public function set_output($out)
 		{
 			$this->output = $out;
@@ -22,6 +31,9 @@
 			return $this;
 		}
 		
+		/**
+		 * Appends a string of data to the output
+		 */
 		public function append_output($out)
 		{
 			$this->output .= $out;
@@ -29,11 +41,17 @@
 			return $this;
 		}
 		
+		/**
+		 * Clears the current output
+		 */
 		public function clear_output()
 		{
 			return $this->set_output("");
 		}
 		
+		/**
+		 * TODO prints the final output out
+		 */
 		function __destruct()
 		{
 			echo $this->get_output();
