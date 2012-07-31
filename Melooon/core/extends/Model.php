e1dc137506c45095fa89bd6fb6eaa7ad4c187c9c
Jacob Marshall <jacob.christian.marshall@gmail.com>
2012-07-31 17:54:25 +1200

    Added the _get() function to Model for easy use of the Controller instance.

--- Melooon/core/extends/Model.php
+++ Melooon/core/extends/Model.php
@@ -2,10 +2,16 @@
 
 	class Model
 	{
-		
+	
 		function __construct()
 		{
 			// Nothing yet :)
+		}
+		
+		public function _get($sub)
+		{
+			$m =& get_instance();
+			return $m->$sub;
 		}
 		
 	}
